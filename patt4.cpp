dbd1ecae1901673d83c26168209769c88bb74065
deepesh123github <133794619+deepesh123github@users.noreply.github.com>
2023-09-08 09:31:39 -0600

    Add files via upload

---
+++ patt4.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n; 
+     cout<<"enter a number =";
+    cin>>n; 
+        for(int i =1 ; i<=n ; i++) {
+for(int j =1 ; j<=i ; j++) {
+ cout<<i;
+    }  
+     cout<<endl;
+    }
+    return 0;
+}
+
+/*
+output
+enter a number =4
+1
+22
+333
+4444
+*/ 
